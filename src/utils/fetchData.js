373b727630c83c8d5e2d397d804169f2a3487640
CodeExcalibar <Alex.raju1010@gmail.com>
2024-08-07 17:31:04 +0100

    removed the seperator

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,16 @@
 export default async function fetchData(version, endpoint, page = 1) {
 	try {
 		// await new Promise((resolve) => setTimeout(resolve, 3000));
-		const separator = endpoint.includes("?") ? "&" : "?";
+		// const separator = endpoint.includes("?") ? "&" : "?";
 
-		const res = await fetch(
-			`https://api.themoviedb.org/${version}/${endpoint}${separator}page=${page}`,
-			{
-				cache: "no-store",
-				headers: {
-					accept: "application/json",
-					Authorization:
-						"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTA4Y2ZlYjY1ZWExMWEwMGZkMWE1MmZmMmFlMTI0ZCIsInN1YiI6IjY0OWVmZmQ5MDkxZTYyMDBlYjdiZGY0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mbunsoSvSxZCheT49skOZk2hX1TCwxpjAvw0ntVBLhs",
-				},
-			}
-		);
+		const res = await fetch(`https://api.themoviedb.org/${version}/${endpoint}`, {
+			cache: "no-store",
+			headers: {
+				accept: "application/json",
+				Authorization:
+					"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNTA4Y2ZlYjY1ZWExMWEwMGZkMWE1MmZmMmFlMTI0ZCIsInN1YiI6IjY0OWVmZmQ5MDkxZTYyMDBlYjdiZGY0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mbunsoSvSxZCheT49skOZk2hX1TCwxpjAvw0ntVBLhs",
+			},
+		});
 		if (!res.ok) {
 			throw new Error("Failed to fetch data");
 			// const errorData = await res.json(); // Assuming error details in JSON
