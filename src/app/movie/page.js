abe88614e82ccc1c598a46ea23223cead51c7926
CodeExcalibar <Alex.raju1010@gmail.com>
2024-08-09 11:59:24 +0100

    Adding suspense to movies page

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -15,13 +15,13 @@ export default async function MoviesPage() {
 	return (
 		<div className={styles.container} style={{ animation: "fadeIn 0.5s ease-in-out" }}>
 			<h2>Movies List</h2>
-			{/* <Suspense fallback={<LoadingSkeletons />}> */}
-			<div className={styles.movieContainer}>
-				{movies.results.map((movie) => (
-					<MediaCard key={movie.id} media={movie} mediaMode={"movie"} />
-				))}
-			</div>
-			{/* </Suspense> */}
+			<Suspense fallback={<LoadingSkeletons />}>
+				<div className={styles.movieContainer}>
+					{movies.results.map((movie) => (
+						<MediaCard key={movie.id} media={movie} mediaMode={"movie"} />
+					))}
+				</div>
+			</Suspense>
 		</div>
 	);
 }
