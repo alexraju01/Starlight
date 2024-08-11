465450719de436940143e2cf772015759d8518b3
CodeExcalibar <Alex.raju1010@gmail.com>
2024-08-12 00:39:22 +0100

    Optimizing the card overview

--- src/components/MediaCard/MediaCard.js
+++ src/components/MediaCard/MediaCard.js
@@ -6,27 +6,29 @@ import Link from "next/link";
 
 export default function MediaCard({ media, mediaMode }) {
 	const [isLoaded, setIsLoaded] = useState(false);
+	const { id, name, title, poster_path } = media;
+	const displayName = title || name;
 
 	useEffect(() => {
 		// Logic to handle loading state and animation until the image is fully loaded
-		if (!media.poster_path) setIsLoaded(true);
-	}, [media.poster_path]);
+		if (!poster_path) setIsLoaded(true);
+	}, [poster_path]);
 
 	return (
-		<Link href={`/${mediaMode}/${media.id}`}>
+		<Link href={`/${mediaMode}/${id}`}>
 			<div className={styles.mediaCard}>
 				{!isLoaded && <div className={styles.pulse}></div>}
 				<Image
 					className={`${styles.img} ${isLoaded ? styles.loaded : ""}`}
 					src={
-						media.poster_path
-							? `https://image.tmdb.org/t/p/w342${media.poster_path}`
+						poster_path
+							? `https://image.tmdb.org/t/p/w342${poster_path}`
 							: `https://image.tmdb.org/t/p/w342/`
 					}
 					width={70}
 					height={105}
 					layout="responsive"
-					alt={media.title}
+					alt={displayName}
 					priority={true}
 					// loading="lazy"
 					onLoadingComplete={() => setIsLoaded(true)}
--- src/components/MediaOverview/MediaOverview.js
+++ src/components/MediaOverview/MediaOverview.js
@@ -1,4 +1,3 @@
-// import fetchData from "@/app/utils/fetchData";
 import Image from "next/image";
 import styles from "./MediaOverview.module.css";
 import fetchData from "@/utils/fetchData";
@@ -8,76 +7,82 @@ import { displayRuntime } from "@/utils/displayRuntime";
 import CastContainer from "../Cast/CastContainer";
 import GoBack from "../GoBack/GoBack";
 import SimilarMedia from "../SimilarMedia/SimilarMedia";
-import Slider from "../Slider/Slider";
 import Icons from "@/utils/icons";
 import Link from "next/link";
 import MediaCard from "../MediaCard/MediaCard";
 
 export default async function MediaOverview({ params, mediaMode }) {
-	const [mediaDetails, casts] = await Promise.all([
+	const [mediaDetails, { cast }] = await Promise.all([
 		fetchData(3, `/${mediaMode}/${params.slug}`),
 		fetchData(3, `/${mediaMode}/${params.slug}/credits`),
 	]);
-	console.log(mediaDetails);
+
+	const {
+		backdrop_path,
+		poster_path,
+		title,
+		name,
+		release_date,
+		first_air_date,
+		media_type,
+		genres = [],
+		number_of_seasons,
+		number_of_episodes,
+		runtime,
+		episode_run_time,
+		overview,
+		vote_average,
+	} = mediaDetails;
+
+	const mediaTitle = title || name;
+	const releaseDate = release_date || first_air_date;
+	const mediaSrc = backdrop_path
+		? `https://image.tmdb.org/t/p/original${backdrop_path}`
+		: `https://image.tmdb.org/t/p/original${poster_path}`;
+
+	const renderSeasonEpisodeInfo = number_of_seasons && number_of_episodes && (
+		<div className={styles.mediaMetadata}>
+			<div>{`${number_of_seasons} Seasons`}</div>
+			<div>{`${number_of_episodes} Episodes`}</div>
+		</div>
+	);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.imgContainer}>
-				<Image
-					className={styles.movieBackground}
-					fill
-					alt={mediaDetails.title || mediaDetails.name}
-					src={
-						mediaDetails.backdrop_path
-							? `https://image.tmdb.org/t/p/original${mediaDetails.backdrop_path}`
-							: `https://image.tmdb.org/t/p/original${mediaDetails.poster_path}`
-					}
-				/>
+				<Image className={styles.movieBackground} fill alt={mediaTitle} src={mediaSrc} />
 			</div>
 			<GoBack />
 			<div className={styles.Introduction}>
-				<h1 className={styles.title}>{mediaDetails.title || mediaDetails.name}</h1>
+				<h1 className={styles.title}>{mediaTitle}</h1>
 				<div className={styles.blurBox}>
 					<div className={styles.posterContainer}>
-						<MediaCard media={mediaDetails} mediaMode={mediaDetails.media_type} />
+						<MediaCard media={{ backdrop_path, poster_path }} mediaMode={media_type} />
 					</div>
 					<div className={styles.stat}>
-						{mediaDetails.release_date || mediaDetails.first_air_date ? (
-							<p className={styles.date}>
-								{dateConverter(mediaDetails.release_date || mediaDetails.first_air_date)}
-							</p>
-						) : (
-							<p>----</p>
-						)}
-						<p>{displayRuntime(mediaDetails)} </p>
-
-						<RatingIcon mediaDetails={mediaDetails} />
+						<p className={styles.date}>{releaseDate ? dateConverter(releaseDate) : "----"}</p>
+						<p>{displayRuntime({ episode_run_time, runtime })}</p>
+						<RatingIcon mediaDetails={vote_average} />
 					</div>
 
-					{/*  ========= genre ========= */}
 					<div className={styles.genreList}>
-						{mediaDetails.genres.map((genre, index) => (
-							<Link key={index} href={`/genre/${genre.id}`}>
+						{genres.map((genre) => (
+							<Link key={genre.id} href={`/genre/${genre.id}`}>
 								<p>{genre.name}</p>
 							</Link>
 						))}
 					</div>
 
-					{mediaDetails.number_of_seasons && mediaDetails.number_of_episodes ? (
-						<div className={styles.mediaMetadata}>
-							<div>{`${mediaDetails.number_of_seasons} Seasons`}</div>
-							<div>{`${mediaDetails.number_of_episodes} Episodes`}</div>
-						</div>
-					) : null}
+					{renderSeasonEpisodeInfo}
 
-					<p className={styles.description}>{mediaDetails.overview}</p>
+					<p className={styles.description}>{overview}</p>
 					<div className={styles.watchBtn}>
 						<i>{Icons.filmPlay}</i>
 						<p>Watch Now</p>
 					</div>
 				</div>
 			</div>
-			<CastContainer castList={casts.cast.slice(0, 10)} />
+			<CastContainer castList={cast.slice(0, 10)} />
 			<SimilarMedia mediaMode={mediaMode} params={params} />
 		</div>
 	);
