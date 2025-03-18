b45a4c812aa124bb11b563a2b21929e100b673f9
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-18 20:38:54 +0000

    Updated README.md

--- src/components/Sidebar/NavLinks/NavLinks.js
+++ src/components/Sidebar/NavLinks/NavLinks.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./NavLinks.module.css";
 
-export default function NavLinks({ linkData: { href, icon, name }, closeSidebar }) {
+export default function NavLinks({
+	linkData: { href, icon, name },
+	closeSidebar,
+}) {
 	const path = usePathname();
 
 	const handleClick = () => {
@@ -13,8 +16,12 @@ export default function NavLinks({ linkData: { href, icon, name }, closeSidebar 
 	};
 
 	return (
-		<li>
-			<Link className={path === href ? styles.active : ""} href={href} onClick={handleClick}>
+		<li role="listitem">
+			<Link
+				className={path === href ? styles.active : ""}
+				href={href}
+				onClick={handleClick}
+			>
 				<i>{icon}</i>
 				<p>{name}</p>
 			</Link>
