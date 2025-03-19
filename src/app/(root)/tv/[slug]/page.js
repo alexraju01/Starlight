bb7e0845117d146a168ea437cff55c028e4fca25
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-19 00:00:33 +0000

    Upgraded to Next.js 15

--- src/app/(root)/tv/[slug]/page.js
+++ src/app/(root)/tv/[slug]/page.js
@@ -3,10 +3,11 @@ import styles from "./tvDetail.module.css";
 import MediaOverview from "../../../../components/MediaOverview/MediaOverview";
 import Spinner from "../../../../components/LoadingSkeletons/Spinner/Spinner";
 
-export default async function page({ params }) {
-	const mediaMode = "tv";
+export default async function page(props) {
+    const params = await props.params;
+    const mediaMode = "tv";
 
-	return (
+    return (
 		<section className={styles.imgContainer}>
 			<Suspense fallback={<Spinner />}>
 				<MediaOverview params={params} mediaMode={mediaMode} />
