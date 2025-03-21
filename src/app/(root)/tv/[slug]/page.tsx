5cc5db557f3ccd47fb7bb13eee65dc508e17dfde
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-21 10:01:37 +0000

    fix: Installed the missing dev dependencies and ts issues

--- src/app/(root)/tv/[slug]/page.tsx
+++ src/app/(root)/tv/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import styles from "./tvDetail.module.css";
 import Spinner from "@/components/LoadingSkeletons/Spinner/Spinner";
 import MediaOverview from "@/components/MediaOverview/MediaOverview";
+import { MediaMode } from "@/types/mediaMode";
 
 interface Props {
 	params: Promise<{ slug: string }>;
@@ -9,12 +10,11 @@ interface Props {
 
 export default async function page({ params }: Props) {
 	const { slug } = await params;
-	const mediaMode = "tv";
 
 	return (
 		<section className={styles.imgContainer}>
 			<Suspense fallback={<Spinner />}>
-				<MediaOverview params={slug} mediaMode={mediaMode} />
+				<MediaOverview params={slug} mediaMode={MediaMode.TV} />
 			</Suspense>
 		</section>
 	);
