7ba8e871a3dbfaf602b2a258bb6521449d33d073
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-21 15:14:26 +0000

    chore: Removed the _ before the loading.tsx

--- src/app/(root)/movie/page.tsx
+++ src/app/(root)/movie/page.tsx
@@ -1,6 +1,6 @@
-import { Suspense } from "react";
+// import { Suspense } from "react";
 
-import { LoadingSkeletons } from "@/components";
+// import { LoadingSkeletons } from "@/components";
 import MediaWrapper from "@/components/MediaHandlers/MediaWrapper";
 import { MediaMode } from "@/types/mediaMode";
 
@@ -10,9 +10,9 @@ export default function MoviesPage() {
 	return (
 		<div className={styles.container} style={{ animation: "fadeIn 0.5s ease-in-out" }}>
 			<h2>Movies List</h2>
-			<Suspense fallback={<LoadingSkeletons />}>
-				<MediaWrapper mediaMode={MediaMode.Movie} />
-			</Suspense>
+			{/* <Suspense fallback={<LoadingSkeletons />}> */}
+			<MediaWrapper mediaMode={MediaMode.Movie} />
+			{/* </Suspense> */}
 		</div>
 	);
 }
