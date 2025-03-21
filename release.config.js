51cc16155038a60f336079ada9972a6499411470
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-21 15:54:16 +0000

    chore: Modified workflow

--- release.config.js
+++ release.config.js
@@ -1,17 +1,17 @@
-// release.config.js
 module.exports = {
+	repositoryUrl: "https://github.com/alexraju01/Starlight.git",
 	branches: ["main"],
 	plugins: [
 		"@semantic-release/commit-analyzer",
 		"@semantic-release/release-notes-generator",
 		"@semantic-release/changelog",
+		"@semantic-release/github",
 		[
 			"@semantic-release/git",
 			{
-				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				assets: ["CHANGELOG.md", "package.json"],
 				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
 			},
 		],
-		"@semantic-release/github",
 	],
 };
