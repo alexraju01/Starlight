cd73fad8ce2fbf1c035925c68f387e65ba65a3aa
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-21 15:49:10 +0000

    chore: modified release.config

--- release.config.js
+++ release.config.js
@@ -1,23 +1,17 @@
+// release.config.js
 module.exports = {
-    branches: ["main"],
-    plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
-      "@semantic-release/changelog",
-      "@semantic-release/npm",
-      [
-        "@semantic-release/github",
-        {
-          assets: ["out/**"] // Adjust for your Next.js build output
-        }
-      ],
-      [
-        "@semantic-release/git",
-        {
-          assets: ["package.json", "CHANGELOG.md"],
-          message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
-        }
-      ]
-    ]
-  };
-  
+	branches: ["main"],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/changelog",
+		[
+			"@semantic-release/git",
+			{
+				assets: ["CHANGELOG.md", "package.json", "package-lock.json"],
+				message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+			},
+		],
+		"@semantic-release/github",
+	],
+};
