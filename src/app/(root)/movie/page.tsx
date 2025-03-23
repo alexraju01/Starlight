e6b7ca1f656972781fe275b3bc63edc6ac404491
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-23 16:54:42 +0000

    refactor: Removed CSS modules files, instead used Tailwind CSS styles for the Movies and TVs page

--- src/app/(root)/movie/page.tsx
+++ src/app/(root)/movie/page.tsx
@@ -1,18 +1,11 @@
-// import { Suspense } from "react";
-
-// import { LoadingSkeletons } from "@/components";
 import MediaWrapper from "@/components/MediaHandlers/MediaWrapper";
 import { MediaMode } from "@/types/mediaMode";
 
-import styles from "./page.module.css";
-
 export default function MoviesPage() {
 	return (
-		<div className={styles.container} style={{ animation: "fadeIn 0.5s ease-in-out" }}>
-			<h2>Movies List</h2>
-			{/* <Suspense fallback={<LoadingSkeletons />}> */}
+		<div className='flex flex-col mt-16 w-full animate-fadeIn'>
+			<h2 className='text-2xl text-center'>Movies List</h2>
 			<MediaWrapper mediaMode={MediaMode.Movie} />
-			{/* </Suspense> */}
 		</div>
 	);
 }
