0551a78e2e4e01c5518a944aa0998ce2eb21963f
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-23 16:54:42 +0000

    refactor: Removed CSS modules files, instead used Tailwind CSS styles for the Movies and TVs page

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,5 @@
 import { ButtonHTMLAttributes, ReactNode } from "react";
 
-import styles from "./Button.module.css";
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 	icon?: ReactNode;
 	children: ReactNode;
@@ -9,7 +7,15 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button({ icon, children, ...props }: ButtonProps) {
 	return (
-		<button className={styles.btn} {...props}>
+		<button
+			className={`
+        flex items-center gap-4 bg-white text-black border-none
+        px-5 py-2.5 text-lg font-bold rounded-xl cursor-pointer
+        transition-all duration-300 ease-in-out mb-6
+        hover:scale-110
+        disabled:opacity-25 disabled:cursor-not-allowed
+      `}
+			{...props}>
 			{icon && <i>{icon}</i>}
 			<p>{children}</p>
 		</button>
