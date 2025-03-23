ba069275946d1d4bce79141c8e2d881ef144c8e8
CodeExcalibar <Alex.raju1010@gmail.com>
2025-03-23 15:01:20 +0000

    refactor: Removed CSS modules instead used Tailwind CSS styles for the home page

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,7 +1,6 @@
 "use client";
-import { useState } from "react";
 
-import styles from "./SearchBox.module.css";
+import { useState } from "react";
 import SearchCard from "../SearchCard/SearchCard";
 
 export default function SearchBox() {
@@ -10,14 +9,15 @@ export default function SearchBox() {
 	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
 		setSearch(e.target.value);
 	};
+
 	return (
-		<div className={styles.container}>
+		<div className='w-full relative max-w-[40rem] min-w-[25rem] md:w-1/2'>
 			<input
-				className={styles.searchBox}
-				onChange={handleSearch}
 				type='text'
 				placeholder='Search...'
 				value={search}
+				onChange={handleSearch}
+				className='w-full h-16 border border-white/20 rounded-full bg-[#100f10] text-white px-4 text-[1.4rem] focus:outline-none'
 			/>
 			<SearchCard query={search} />
 		</div>
