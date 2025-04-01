27e62c1e81b1798d626102cf4429a3b17e944121
UP2014957 <91965953+UP2014597@users.noreply.github.com>
2025-04-01 22:21:57 +0100

    fix: Testing release changes again

--- release.config.js
+++ release.config.js
@@ -24,37 +24,18 @@ module.exports = {
 		[
 			"@semantic-release/release-notes-generator",
 			{
-			  preset: "conventionalcommits",
-			  presetConfig: {
-				types: [
-				  { type: "feat", section: "✨ Features" },
-				  { type: "fix", section: "🐛 Bug Fixes" },
-				  { type: "refactor", section: "🛠 Refactors" },
-				  { type: "upgrade", section: "📦 Upgrades" },
-				  { type: "chore", hidden: true },
-				],
-			  },
-			  writerOpts: {
-				transform: (commit, context) => {
-				  const issues = [];
-		  
-				  // Create a link to the commit
-				  if (commit.hash && context.repository && context.host) {
-					const shortHash = commit.hash.substring(0, 7);
-					const commitUrl = `${context.host}/${context.owner}/${context.repository}/commit/${commit.hash}`;
-					commit.shortHash = shortHash;
-					commit.commitUrl = commitUrl;
-				  }
-		  
-				  // Format the message
-				  commit.subject = `* ${commit.subject} (${commit.commitUrl})`;
-		  
-				  return commit;
-				},
-			  },
+				preset: "conventionalcommits",
+			    presetConfig: {
+					types: [
+					  { type: "feat", section: "✨ Features" },
+					  { type: "fix", section: "🐛 Bug Fixes" },
+					  { type: "refactor", section: "🛠 Refactors" },
+					  { type: "upgrade", section: "📦 Upgrades" },
+					  { type: "chore", hidden: true },
+					],
+				  },
 			},
-		  ]
-,		  
+		],
 		"@semantic-release/changelog",
 		"@semantic-release/github",
 		[
