08a5f38cb80ebd7e8ddd2d2798b8d1849891eea3
alexraju01 <alex.raju1010@gmail.com>
2025-04-01 23:19:30 +0100

    fix: testing changes on release config

--- release.config.js
+++ release.config.js
@@ -22,18 +22,41 @@ module.exports = {
 		[
 			"@semantic-release/release-notes-generator",
 			{
-				preset: "conventionalcommits",
-			    presetConfig: {
-					types: [
-					  { type: "feat", section: "✨ Features" },
-					  { type: "fix", section: "🐛 Bug Fixes" },
-					  { type: "refactor", section: "🛠 Refactors" },
-					  { type: "upgrade", section: "📦 Upgrades" },
-					  { type: "chore", hidden: true },
-					],
-				  },
+			  preset: "conventionalcommits",
+			  presetConfig: {
+				types: [
+				  { type: "feat", section: "✨ Features" },
+				  { type: "fix", section: "🐛 Bug Fixes" },
+				  { type: "refactor", section: "🛠 Refactors" },
+				  { type: "upgrade", section: "📦 Upgrades" },
+				  { type: "chore", hidden: true },
+				],
+			  },
+			  writerOpts: {
+				transform: (commit, context) => {
+				  const issues = [];
+		  
+				  // Format commit message
+				  let message = commit.header;
+		  
+				  if (commit.hash) {
+					const shortHash = commit.hash.substring(0, 7);
+					const commitUrl = `${context.repository.url}/commit/${commit.hash}`;
+					message += ` ([\`${shortHash}\`](${commitUrl}))`;
+				  }
+		  
+				  commit.subject = message;
+		  
+				  return commit;
+				},
+				groupBy: "type",
+				commitGroupsSort: "title",
+				commitsSort: ["scope", "subject"],
+				noteGroupsSort: "title",
+			  },
 			},
-		],
+		  ],
+		  
 		"@semantic-release/changelog",
 		"@semantic-release/github",
 		[
