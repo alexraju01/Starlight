7e0682882c05fd517faa389d3cbb1c78f028e21f
CodeExcalibar <Alex.raju1010@gmail.com>
2025-08-29 18:00:12 +0100

    fix: Fixed Upcoming title spacing and the media mode badge

--- src/app/(root)/movie/page.tsx
+++ src/app/(root)/movie/page.tsx
@@ -4,7 +4,7 @@ import { MediaMode } from "@/types/mediaMode";
 export default function MoviesPage() {
 	return (
 		<div className='flex flex-col mt-54 md:mt-40 w-full animate-fadeIn'>
-			<h2 className='text-2xl mb-10 text-center'>Movies List</h2>
+			<h2 className='text-3xl mb-10 text-center'>Movies List</h2>
 			<MediaWrapper mediaMode={MediaMode.MOVIE} />
 		</div>
 	);
--- src/app/(root)/upcoming/page.tsx
+++ src/app/(root)/upcoming/page.tsx
@@ -3,18 +3,15 @@ import { Suspense } from "react";
 import { Spinner } from "@/components/Feedback/LoadingSkeletons";
 import { MediaMode } from "@/types/mediaMode";
 
-import styles from "./Upcoming.module.css";
 import MediaWrapper from "@/components/Media/MediaHandlers/MediaWrapper";
 
 export default async function UpcomingPage() {
 	return (
 		<div className='flex flex-col justify-center items-center mt-60 md:mt-30'>
-			<h2>Upcoming shows</h2>
-			<div className={styles.upcomingContainer}>
-				<Suspense fallback={<Spinner />}>
-					<MediaWrapper mediaMode={MediaMode.UPCOMING} />
-				</Suspense>
-			</div>
+			<h2 className=' text-3xl mt-54 md:mt-40'>Upcoming shows</h2>
+			<Suspense fallback={<Spinner />}>
+				<MediaWrapper mediaMode={MediaMode.UPCOMING} />
+			</Suspense>
 		</div>
 	);
 }
