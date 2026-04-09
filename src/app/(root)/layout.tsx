a0e700f7bd4aac5a57844c241a2510e8b1f19e3d
alexraju01 <alex.raju1010@gmail.com>
2026-04-09 21:34:17 +0100

    using the globals bg-secondary calss

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -4,7 +4,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavBar />
-      <main className="overflow-auto w-full h-full mt-20 lg:mt-[70px] xl:mt-[87px]">
+      <main className="overflow-auto w-full h-full mt-20 lg:mt-[70px] xl:mt-[87px] bg-secondary">
         {children}
       </main>
       <Footer />
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     <>
       <Carousel mediaMode={MediaMode.MOVIE} />
 
-      <section className="relative sm:mt-15 bg-[#100F10] overflow-hidden">
+      <section className="relative sm:mt-15  overflow-hidden">
         <BlurBackground />
 
         <div className="md:mx-6 lg:mx-[68px] 2xl:mx-[101px] px-2">
